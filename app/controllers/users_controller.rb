fc3e5c9f57f2f257c3131e63af5748fb3b36f268
Dustin Smith <dustinsmith1024@hotmail.com>
2011-09-17 12:36:39 -0500

    changed controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, :notice => 'User was successfully created.' }
-        format.json { render :json => @user, :status => :created, :callback => params[:callback] }
+        format.json { render :json => @user, :status => :created }
       else
         format.html { render :action => "new" }
         format.json { render :json => @user.errors, :status => :unprocessable_entity }
