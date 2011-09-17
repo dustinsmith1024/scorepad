ff1eb224693d238e132af5ce33ade2db78a28f72
Dustin Smith <dustinsmith1024@hotmail.com>
2011-09-17 13:49:44 -0500

    messing with jsonp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @users }
+      format.jsonp { render :json => @users, :callback => params[:callback] }
     end
   end
 
