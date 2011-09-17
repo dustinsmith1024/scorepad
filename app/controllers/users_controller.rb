8c0208c3b6dd93db96a141f25b627833ddfc5c19
Dustin Smith <dustinsmith1024@hotmail.com>
2011-09-17 14:32:34 -0500

    added callback to user request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render :json => @user }
+      format.json { render :json => @user, :callback => params[:callback] }
     end
   end
 
